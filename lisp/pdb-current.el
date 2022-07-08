40b01e9c7c23fc1dccc8e7d48e2fda97a1d26dee
pawciobiel <pawciobiel@gmail.com>
2022-07-08 13:01:35 +0200

    Reformated lisp/

--- lisp/pdb-current.el
+++ lisp/pdb-current.el
@@ -1,5 +1,5 @@
 ;;pdb setup, note the python version
-(setq pdb-path '/usr/local/lib/python2.7/pdb.py
+(setq pdb-path '/usr/lib/python3.8/pdb.py
        gud-pdb-command-name (symbol-name pdb-path))
  (defadvice pdb (before gud-query-cmdline activate)
    "Provide a better default command line when called interactively."
